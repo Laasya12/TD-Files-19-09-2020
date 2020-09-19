3cefa1f1a01c01c5efcb1281e7d89b3a89f9a25a
Laasya12 <71327980+Laasya12@users.noreply.github.com>
2020-09-19 16:31:14 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'E -Commerce Project with Java'
+copyright = '2020, Laasya'
+author = 'Laasya'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
